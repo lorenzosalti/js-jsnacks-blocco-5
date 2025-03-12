7037f6a6b1df7978e98dfc1c6037491e1e9fdfb1
lorenzosalti <lorenzosalti@gmail.com>
2025-03-12 15:22:36 +0100

    feat: snack 5 with array method

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,11 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const evenNums = nums.filter((num) => {
+  if (num % 2 === 0) {
+    return num;
+  }
+});
+
+// console.log(evenNums);
