d88ffbfb2ff54e3fcf217398e02a989ca84faa58
lorenzosalti <lorenzosalti@gmail.com>
2025-03-12 17:08:31 +0100

    feat: snack 6 with array method (2 filter)

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,19 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const zucchineLunghe = zucchine.filter((zucchina) => {
+  if (zucchina['length'] > 15) {
+    return zucchina;
+  }
+});
+
+// console.log(zucchineLunghe);
+
+const zucchineCorte = zucchine.filter((zucchina) => {
+  if (zucchina['length'] <= 15) {
+    return zucchina;
+  }
+});
+
+// console.log(zucchineCorte);
